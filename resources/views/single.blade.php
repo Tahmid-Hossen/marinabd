46d6b1383b522897058efeb586e5675c0549b414
Tahmid-Hossen <aryantahmid11@gmail.com>
2020-10-23 10:10:33 +0600

    some section are added

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,77 +1,76 @@
 @extends('default')
 @section('content')
-<div class="single mt-5">
-    <div class="container">
-        <div class="form-header mb-5 text-center">
-            <h1>Book your favourite single room.</h1>
-        </div>
-        <div class="row">
-            <div class="col-sm-8">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <img src="images/room1.jpg" alt="">
-                    </div>
-                    <div class="col-sm-6">
-                        <img src="images/room1.jpg" alt="">
-                    </div>
-                </div>
-                <div class="row mt-4">
-                    <div class="col-sm-6">
-                        <img src="images/room1.jpg" alt="">
-                    </div>
-                    <div class="col-sm-6">
-                        <img src="images/room1.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <!-- Registration area -->
-                <div id="reserve" class="section">
-                    <div class="container">
-                        <div class="row">
-        <div class="col-sm-12 col-lg-offset-3">
-              <form action="{{ route('store.registration') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group">
-                  <label for="name">Enter your full name</label>
-                  <input required type="text" name="name" class="form-control" placeholder="Enter your full name">
-                </div>
-
-                <div class="form-group">
-                  <label for="id">Email</label>
-                  <input required type="text" name="email" class="form-control" placeholder="Enter your email">
-                </div> 
-              <div class="form-group">
-                  <label for="batch">Phone</label>
-                  <input required type="phone" name="phone" class="form-control" placeholder="Enter your phone">
-                </div> 
-
-                <div class="form-group">
-                  <label for="name">Total member</label>
-                  <input required type="number" name="member" class="form-control" placeholder="Enter your full name">
-                </div>
-              <div class="form-group">
-                  <label for="name">Arrive Date</label>
-                  <input required type="date" name="a_date" class="form-control" placeholder="when you will come">
-                </div>
-              <div class="form-group">
-                  <label for="l_date">Leave Date</label>
-                  <input required type="date" name="l_date" class="form-control" placeholder="when you will leave the hotel">
-                </div>
-                <div class="form-group">
-                  <label for="l_date">Address</label>
-                  <!-- <input required type="text" name="address" class="form-control" placeholder="type your present address"> -->
-                  <textarea name="text" cols="40" rows="5"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary btn-lg btn-block mb-2">Submit</button>
-            </form>
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-12">
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Room Floor list</th>
+              <th scope="col">Single Room number</th>
+              <th scope="col">Double Room number</th>
+              <th scope="col">tripple Room number</th>
+              <th scope="col">Familly Room number</th>
+              <th scope="col">Booked Room</th>
+              <th scope="col">Free Room</th>
+              <th scope="col">Reserve Your Room</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Ist Floor</th>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>102, 103</td>
+              <td>101</td>
+              <td><a href="" class="btn btn-primary">Reserve</a></td>
+            </tr>
+            <tr>
+              <th scope="row">2nd Floor</th>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>102, 103</td>
+              <td>101</td>
+              <td><a href="" class="btn btn-primary">Reserve</a></td>
+            </tr>
+            <tr>
+              <th scope="row">3rd Floor</th>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>102, 103</td>
+              <td>101</td>
+              <td><a href="" class="btn btn-primary">Reserve</a></td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+  </div>
+   <h1 class="text-center mb-5">Some single room sample</h1>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="room-image">
+        <img src="images/room1.jpg" alt="">
       </div>
     </div>
-                </div>
 
-                <!-- End registration form -->
-            </div>
-        </div>
+    <div class="col-sm-4">
+      <div class="room-image">
+        <img src="images/room1.jpg" alt="">
+      </div>
     </div>
+
+    <div class="col-sm-4">
+      <div class="room-image">
+        <img src="images/room1.jpg" alt="">
+      </div>
+    </div>
+
+  </div>
 </div>
 @stop
--- resources/views/tripple.blade.php
+++ resources/views/tripple.blade.php
@@ -1,6 +1,76 @@
 @extends('default')
 @section('content')
-<div class="tripple">
-  <h1> This is tripple room. </h1>
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-12">
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Room Floor list</th>
+              <th scope="col">Single Room number</th>
+              <th scope="col">Double Room number</th>
+              <th scope="col">tripple Room number</th>
+              <th scope="col">Familly Room number</th>
+              <th scope="col">Booked Room</th>
+              <th scope="col">Free Room</th>
+              <th scope="col">Reserve Your Room</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Ist Floor</th>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>102, 103</td>
+              <td>101</td>
+              <td><a href="" class="btn btn-primary">Reserve</a></td>
+            </tr>
+            <tr>
+              <th scope="row">2nd Floor</th>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>102, 103</td>
+              <td>101</td>
+              <td><a href="" class="btn btn-primary">Reserve</a></td>
+            </tr>
+            <tr>
+              <th scope="row">3rd Floor</th>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>101, 102, 103</td>
+              <td>102, 103</td>
+              <td>101</td>
+              <td><a href="" class="btn btn-primary">Reserve</a></td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+  </div>
+   <h1 class="text-center mb-5">Some single room sample</h1>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="room-image">
+        <img src="images/room1.jpg" alt="">
+      </div>
+    </div>
+
+    <div class="col-sm-4">
+      <div class="room-image">
+        <img src="images/room1.jpg" alt="">
+      </div>
+    </div>
+
+    <div class="col-sm-4">
+      <div class="room-image">
+        <img src="images/room1.jpg" alt="">
+      </div>
+    </div>
+
+  </div>
 </div>
 @stop
