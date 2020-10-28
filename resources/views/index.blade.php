49aa4632d7f47bc3bc90c08d7c47d4a4b5481bfd
Tahmid-Hossen <aryantahmid11@gmail.com>
2020-10-29 02:48:29 +0600

    heading border implement

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 
 <div class="about-section mt-5">
     <div class="container">
-        <h1 class="text-center mb-5"><span class="test">About US</span></h1>
+        <h1 class="text-center mb-5 subtitle fancy"><span class="test">About US</span></h1>
         <div class="row">
             <div class="col-sm-4">
                 <p class="text-justify">২৩শে অক্টোবর সকাল ৭টার আবহাওয়া বিজ্ঞপ্তিতে জানানো হয় যে উত্তর-পশ্চিম বঙ্গোপসাগরের নিম্নচাপটি উত্তর-পূর্ব দিকে অগ্রসর হয়েছে এবং একই দিকে আরো অগ্রসর হতে পারে। বঙ্গোপসাগরের এই নিম্নচাপটির কারণে বৃহস্পতিবার সকাল থেকে দেশের দক্ষিণাঞ্চলীয় জেলাগুলোতে ভারী বৃষ্টি হয়েছে। রাজধানী ঢাকা সহ মধ্যাঞ্চলীয় জেলাগুলোতেও ভারী থেকে মাঝারি বৃষ্টিপাত হয়েছে। আবহাওয়াবিদ আবদুর রহমান খান জানান, আবহাওয়া অধিদপ্তরের ধারণা অনুযায়ী আজ [শুক্রবার] দুপুর বা সন্ধ্যায় নিম্নচাপটি পশ্চিমবঙ্গ ও বাংলাদেশের ওপর দিয়ে অতিক্রম করতে পারে।এর ফলে শুক্রবার সারাদিন এবং শনিবার দুপুর পর্যন্ত রাজধানী ঢাকা সহ দেশের বিভিন্ন অঞ্চলে বৃষ্টি ও ঝড়ো বাতাস অব্যাহত থাকতে পারে বলে ধারণা প্রকাশ করেন তিনি। তিনি বলেন, "আমাদের অনুমান, আগামীকাল দুপুর থেকে আবহাওয়া ধীরে ধীরে স্বাভাবিক হতে শুরু করতে পারে। রবিবার সকাল থেকে বাতাসের গতিবেগ, বৃষ্টি এবং মেঘ স্বাভাবিক পর্যায়ে চলে আসবে বলে ধারণা করা যেতে পারে।"</p>
@@ -20,7 +20,7 @@
 
 <div class="room-section text-center">
         <div class="container">
-                <h1><span class="test text-center">Our Rooms</span></h1>
+                <h1 class="text-center mb-5 subtitle fancy"><span>Our Rooms</span></h1>
                 <div class="row mt-5 mb-5">
                 <div class="col-sm-3">
                     <div class="room-package">
@@ -74,9 +74,9 @@
 
 
             <!-- Blog Area -->
-            <div class="blog-area">
+            <div class="blog-area mt-5">
             <div class="container">
-                <h1 class="text-center mt-5 mb-5"><span class ="test">Our Recent Blog</span></h1>
+                <h1 class="text-center mb-5 subtitle fancy"><span>Our Recent Blog</span></h1>
                 <div class="row">
                 <div class="col-sm-4">
                     <div class="single-blog">
@@ -123,7 +123,7 @@
             <!-- Video section -->
             <div class="hotel-video mt-5 text-center">
             <div class="container">
-                <h1><span class="test">Recommended Hotel For You</span></h1>
+                <h1 class="text-center mb-5 mt-3 subtitle fancy"><span>Recommended Hotel For You</span></h1>
                 <div class="row">
                 <div class="col-sm-4">
                     <div class="hotel-sidebar">
@@ -190,7 +190,7 @@
             <!-- Testimonial Area -->
             <div class="testimonial-area mt-5 text-center">
             <div class="container">
-                <h1><span class="test">Customer Feedback</span></h1>
+                <h1 class="text-center mb-5 subtitle fancy"><span>Customer Feedback</span></h1>
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="single-testi">
@@ -237,8 +237,8 @@
 
 
 <div class="container mt-5">
-    <h1 class="text-center"><span class="test">Find our location</span></h1>
-    <div class="row">
+    <h1 class="text-center mb-5 mt-3 subtitle fancy"><span>Find our location</span></h1>
+    <div class="row mt-2">
         <div class="col-sm-6">
             <div class="location">
                 <h4 class="mt-3 text-center">Google Map</h4>
